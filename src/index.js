5621efc493607bf209cd553e1ebeb2d9640910ba
eustachekamala <eustachejared25@gmail.com>
2024-07-18 11:16:26 +0300

    I completed a function init where it tell us to create a form of movie, fetch the date of db.json and render it to the page web

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        let input = document.querySelector("#searchByID");
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(res => res.json())
+        .then(data => {
+            title.innerHTML = data.title;
+            summary.innerHTML = data.summary;
+        } )
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+
